6c920774c052a5dc63fd3ac259de0ca42cb6a7d2
Muhammad junaid <161128073+Fsmjb@users.noreply.github.com>
2024-03-12 19:34:30 +0500

    Update 6.04.cpp

--- 6.04.cpp
+++ 6.04.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-	system ("color F0");
 	int a, b;
 	cout << "Enter the number from which start" <<endl;
 	cin  >> a;
 	cout << "Enter the number on which end" << endl; 
 	cin  >> b;
 	
+	if (a%2 != 0 ){
+		++a;
+	}
+	
 	while(a<=b){
 		cout << a << endl;
+		++a;
 		++a;
 	}
 	return 0;
